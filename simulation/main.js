a501594bc0aacef30099ae3b3f593d93ccd6d26c
Dain Saint <dain@cipherprime.com>
2024-10-10 16:58:30 -0400

    simplify community logic

--- simulation/main.js
+++ simulation/main.js
@@ -1,126 +1,6 @@
 import Record from "./js/record.js";
 import simulate from "./simulation.js";
 
-// let parameters = {
-//   communities: 8,
-//   leaderPct: .5,
-// }
-
-// const updatePopulation = () => {
-//   const leaders = document.querySelector(".js-leaders");
-//   const people = document.querySelector(".js-people");
-
-//   updateParameter("leaders", Math.floor(parameters.communities * parameters.leaderPct));
-//   updateParameter("people", parameters.communities - parameters.leaders);
-//   updateParameter("resources", parameters.leaders * parameters.leaderresources + parameters.people * parameters.peopleresources);
-
-//   leaders.innerHTML = parameters.leaders;
-//   people.innerHTML = parameters.people;
-// }
-
-// const updateParameter = (name, value) => {
-//   parameters[name] = value;
-//   saveParameters();
-// }
-
-// const gleanParameters = () => {
-//   document.querySelectorAll("input").forEach(input => parameters[input.name] = input.value);
-// }
-
-// const saveParameters = () => {
-//   localStorage.setItem("parameters", JSON.stringify(parameters) );
-// }
-
-// const loadParameters = () => {
-//   try {
-//     const loaded = localStorage.getItem("parameters");
-//     if( loaded )
-//       parameters = JSON.parse(loaded);
-    
-//     for( var key in parameters ) {
-//       const input = document.querySelector(`input[name="${key}"]`)
-//       if( input )
-//         input.value = parameters[key];
-//     }
-
-//   } catch(e) {
-//     console.log("No save found, using default parameters.");
-//   }
-
-//   return false;
-// }
-
-// const simulate = () => {
-//   // roll
-//   const game = [];
-//   let before = parameters.resources;
-//   let after = parameters.resources;
-
-//   for( let i = 0; i < parameters.rounds; i++ ) {
-//     const roll = [];
-//     const used = Math.min( parameters.action, before );
-//     for( let d = 0; d < used; d++ ) {
-//       const die = Math.floor( 1 + Math.random() * 6 );
-//       roll.push(die);
-//     }
-
-//     const impacted = roll.filter( x => x <= parameters.risk ).length;
-//     const mixed = roll.some(x => x == 4 || x == 5);
-//     const success = roll.some( x => x == 6 );
-//     const crit = roll.filter( x => x == 6).length > 1;
-
-//     let max = roll.reduce( (max, die) => Math.max(max, die), 0) - 1;
-//     if( crit ) max++;
-
-//     const shouldCreate = (success && Math.random() <= parameters.success / 100) 
-//       || (mixed && Math.random() <= parameters.mixed / 100) 
-//       || (crit && Math.random() <= parameters.critsuccess / 100);
-//     let create = shouldCreate ? 1 : 0;
-//     const destroy = Math.floor( parameters.impacted / 100 * impacted );
-
-//     const willpowerCreate = Math.random() <= parameters.create / 100;
-//     const willpowerNumCreate = Math.floor(Math.random() * parameters.numcreate) + 1;
-//     if( willpowerCreate )
-//       create += willpowerNumCreate;
-
-//     const labels = ["Critical Failure", "Mixed Failure", "Mixed Failure", "Mixed Success", "Mixed Success", "Success", "Critical Success"];
-//     const label = labels[max] || "No Resources";
-
-
-//     after = before + create - destroy;
-    
-//     const round = {
-//       stats: {
-//         used,
-//         risk: parameters.risk
-//       },
-//       willpower: {
-//         create: willpowerCreate ? willpowerNumCreate : 0
-//       },
-//       roll,
-//       results: {
-//         label,
-//         impacted,
-//         success,
-//         crit
-//       },
-//       response: {
-//         create,
-//         destroy
-//       },
-//       resources: {
-//         before,
-//         after
-//       }
-//     }
-
-//     game.push( round );
-
-//     before = after;
-//   }
-
-//   return game;
-// }
 
 const printRound = ( round ) => {
   const die = [ "one", "two", "three", "four", "five", "six" ];
@@ -235,7 +115,7 @@ const displayDistribution = (games) => {
 
 }
 
-/*
+
 
 window.addEventListener("DOMContentLoaded", () => {
   // loadParameters();
@@ -324,8 +204,6 @@ window.addEventListener("DOMContentLoaded", () => {
   run();
 })
 
-*/
 
-
-Record.debug = true;
-simulate();
+// Record.debug = true;
+// simulate();
